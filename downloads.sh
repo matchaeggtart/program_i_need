003aef50481499f758055d52849b62aa88d45bd8
matchaeggtart <matchaeggtart@hotmail.com>
2019-06-03 14:24:50 +0800

    Update

--- downloads.sh
+++ downloads.sh
@@ -1,9 +1,26 @@
 cd ~/Downloads
 git clone https://github.com/matchaeggtart/Vim-private.git
+
 mkdir ~/Workspace
+
+# Emacs
 cd ~/Workspace
 git clone https://github.com/matchaeggtart/LearnEmacsin21Days.git
+
+# Notes
 cd ~/Workspace
 git clone https://github.com/matchaeggtart/Notes.git
+
+
+# C
 cd ~/Workspace
 git clone https://github.com/matchaeggtart/C_Primer_Plus.git
+
+
+# Java
+cd ~/Workspace
+git clone https://github.com/matchaeggtart/Java_Study.git
+
+# MySQL
+cd ~/Workspace
+git clone https://github.com/matchaeggtart/LearnMySQL.git
